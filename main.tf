7bbdec893e07730b37e8175cdf04d7888995e733
lochanaarumugam <arucary@gmail.com>
2024-06-17 11:15:36 -0400

    test

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-module "sqs" {
+module "sqs1" {
   source  = "app.terraform.io/arudemoorg/sqs/aws"
   version = "1.0.2"
 
   environment = "dev"
   project = "test-project"
-  queue_name = "aru-demot-q"
+  queue_name = "aru-demot-q1"
+}
+
+module "sqs2" {
+  source  = "app.terraform.io/arudemoorg/sqs/aws"
+  version = "1.0.2"
+
+  environment = "dev"
+  project = "test-project"
+  queue_name = "aru-demot-q2"
 }
