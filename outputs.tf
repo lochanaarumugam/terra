9e35ea39a35cf50828dc6af9a71a52b8020120bd
lochanaarumugam <arucary@gmail.com>
2024-06-17 10:15:21 -0400

    test

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sqs_queue_id" {
-  description = "The ID of the SQS queue"
-  value       = module.sqs.sqs_queue_id
-}
-
-output "sqs_queue_arn" {
-  description = "The ARN of the SQS queue"
-  value       = module.sqs.sqs_queue_arn
-}
